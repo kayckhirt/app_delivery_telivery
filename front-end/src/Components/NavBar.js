bc7d85f86eeda31e9aa40d29246e40bbbac30691
Peterson <petersonpaganelli@outlook.com>
2023-03-29 15:50:26 -0300

    chore: add navbar

---
+++ front-end/src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+// import AppContext from '../Context/AppContext';
+
+function Header() {
+  const history = useHistory();
+  // const { pathname } = history.location;
+
+
+  return (
+    <nav>
+    </nav>
+  );
+}
+
+export default Header;
