12277d9947ecdbe198d13a1c3321358974aa6ce7
julio-silveira <julioqueiroz15@gmail.com>
2023-03-29 17:02:10 -0300

    feat: productCard

--- back-end/src/routes/products.routes.js
+++ back-end/src/routes/products.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-// const productsController = require('../controllers/products.controller');
+const productsController = require('../controllers/products.controller');
 
 const router = Router();
 
-// router.get('/products', productsController);
+router.get('/products/:id', productsController.getById);
+router.get('/products', productsController.getAll);
 
 module.exports = router;
