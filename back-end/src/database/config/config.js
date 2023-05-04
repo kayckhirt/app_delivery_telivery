9d039752be7c263f3a953e73d05880c760896104
kayckhirt <kayckhirt15@gmail.com>
2023-05-04 15:22:06 -0300

    feat: deploy in railway

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
+const environment = process.env.NODE_ENV;
 
 const suffix = {
   prod: '',
@@ -11,12 +11,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.HOSTNAME || process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+    `${process.env.MYSQLDATABASE}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
