69d5234f049a3b49380f2f2e4a4f9fa9b8180d32
Kayck Valentim Hirt <kayckhirt15@gmail.com>
2023-05-05 14:20:28 -0300

    Revert "feat: deploy in railway"

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setupInterceptors } from './interceptors';
 
-const API_URL = process.env.REACT_APP_HOSTNAME;
+const API_URL = `http://${process.env.REACT_APP_HOSTNAME || 'localhost'}:${process.env.REACT_APP_BACKEND_PORT || '3001'}`;
 
 const api = setupInterceptors(
   axios.create({
